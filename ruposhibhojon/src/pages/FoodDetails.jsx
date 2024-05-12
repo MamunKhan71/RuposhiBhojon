8aeb2e6dea25f0993291e599602c0b4c1f8c668f
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-05-12 17:24:55 +0600

    Added Product Details Page

--- ruposhibhojon/src/pages/FoodDetails.jsx
+++ ruposhibhojon/src/pages/FoodDetails.jsx
@@ -1,13 +1,30 @@
 import { BiPurchaseTag } from "react-icons/bi";
 import { Helmet } from "react-helmet";
-import { useLoaderData, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import { useLoaderData } from "react-router-dom";
+import { IoIosTimer } from "react-icons/io";
+import { MdShareLocation } from "react-icons/md";
+import { useState } from "react";
 const FoodDetails = () => {
     const food = useLoaderData().data
+    const [qty, setQty] = useState(0)
+    const handleTimeRemaining = (time) => {
+        const dateAndTime = new Date(time);
+        const dateNow = new Date();
+        const differenceInMilliseconds = dateAndTime.getTime() - dateNow.getTime();
+        const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+        return differenceInDays;
+    }
+    const handleChooseQtyPlus = () => {
+        if (qty < food.food_quantity) {
+            setQty(qty + 1)
+        }
+    }
+    const handleChooseQtyMinus = () => {
+        if (qty > 0) {
+            setQty(qty - 1)
+        }
+    }
 
-    console.log(food);
     return (
         <div>
             <Helmet>
@@ -37,9 +54,8 @@ const FoodDetails = () => {
                                 <div className="flex items-center justify-between gap-6 mb-6">
                                     <div className="text">
                                         <h2 className="font-manrope font-bold text-3xl leading-10  mb-2">
-                                           {food.food_name}
+                                            {food.food_name}
                                         </h2>
-                                        <p className="font-normal text-base text-gray-500">ABS LUGGAGE</p>
                                     </div>
                                     <button className="group transition-all duration-500 p-0.5">
                                         <svg
@@ -71,12 +87,13 @@ const FoodDetails = () => {
                                 <div className="flex flex-col min-[400px]:flex-row min-[400px]:items-center mb-8 gap-y-3">
                                     <div className="flex items-center">
                                         <h5 className="font-manrope font-semibold text-2xl leading-9  ">
-                                            $ 199.00{" "}
+                                            {food.food_quantity}{" "} Pieces Left
                                         </h5>
                                         <span className="ml-3 font-semibold text-lg text-indigo-600">
-                                            30% off
+                                            ( {food.availability ? "In Stock" : "Out of Stock"} )
                                         </span>
                                     </div>
+
                                     <svg
                                         className="mx-5 max-[400px]:hidden"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -87,58 +104,25 @@ const FoodDetails = () => {
                                     >
                                         <path d="M1 0V36" stroke="#E5E7EB" />
                                     </svg>
-                                    <button className="flex items-center gap-1 rounded-xl bg-amber-400 py-1.5 px-2.5 w-max">
-                                        <svg
-                                            width={18}
-                                            height={18}
-                                            viewBox="0 0 18 18"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <g clipPath="url(#clip0_12657_16865)">
-                                                <path
-                                                    d="M8.10326 2.26718C8.47008 1.52393 9.52992 1.52394 9.89674 2.26718L11.4124 5.33818C11.558 5.63332 11.8396 5.83789 12.1653 5.88522L15.5543 6.37768C16.3746 6.49686 16.7021 7.50483 16.1086 8.08337L13.6562 10.4738C13.4205 10.7035 13.313 11.0345 13.3686 11.3589L13.9475 14.7343C14.0877 15.5512 13.2302 16.1742 12.4966 15.7885L9.46534 14.1948C9.17402 14.0417 8.82598 14.0417 8.53466 14.1948L5.5034 15.7885C4.76978 16.1742 3.91235 15.5512 4.05246 14.7343L4.63137 11.3589C4.68701 11.0345 4.57946 10.7035 4.34378 10.4738L1.89144 8.08337C1.29792 7.50483 1.62543 6.49686 2.44565 6.37768L5.8347 5.88522C6.16041 5.83789 6.44197 5.63332 6.58764 5.33818L8.10326 2.26718Z"
-                                                    fill="white"
-                                                />
-                                                <g clipPath="url(#clip1_12657_16865)">
-                                                    <path
-                                                        d="M8.10326 2.26718C8.47008 1.52393 9.52992 1.52394 9.89674 2.26718L11.4124 5.33818C11.558 5.63332 11.8396 5.83789 12.1653 5.88522L15.5543 6.37768C16.3746 6.49686 16.7021 7.50483 16.1086 8.08337L13.6562 10.4738C13.4205 10.7035 13.313 11.0345 13.3686 11.3589L13.9475 14.7343C14.0877 15.5512 13.2302 16.1742 12.4966 15.7885L9.46534 14.1948C9.17402 14.0417 8.82598 14.0417 8.53466 14.1948L5.5034 15.7885C4.76978 16.1742 3.91235 15.5512 4.05246 14.7343L4.63137 11.3589C4.68701 11.0345 4.57946 10.7035 4.34378 10.4738L1.89144 8.08337C1.29792 7.50483 1.62543 6.49686 2.44565 6.37768L5.8347 5.88522C6.16041 5.83789 6.44197 5.63332 6.58764 5.33818L8.10326 2.26718Z"
-                                                        fill="white"
-                                                    />
-                                                </g>
-                                            </g>
-                                            <defs>
-                                                <clipPath id="clip0_12657_16865">
-                                                    <rect width={18} height={18} fill="white" />
-                                                </clipPath>
-                                                <clipPath id="clip1_12657_16865">
-                                                    <rect width={18} height={18} fill="white" />
-                                                </clipPath>
-                                            </defs>
-                                        </svg>
-                                        <span className="text-base font-medium text-white">4.8</span>
-                                    </button>
+                                    <div className="flex items-center gap-2">
+                                        <img className="w-full h-10 rounded-xl ring-primary ring-offset-base-100" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                        <p className="text-base font-bold">{food.donator.userName}</p>
+                                    </div>
                                 </div>
-                                <p className="font-medium text-lg  mb-2">Color</p>
-                                <p>5 Pices</p>
-                                <p className="font-medium text-lg  mb-2">Size (KG)</p>
-                                <div className="grid grid-cols-2 min-[400px]:grid-cols-4 gap-3 mb-3 min-[400px]:mb-8">
-                                    <button className="border border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
-                                        Full Set
+                                <div className="pb-4">
+                                    <p>{food.additional_notes}</p>
+                                </div>
+                                <div className="grid grid-cols-3 gap-3 mb-3 min-[400px]:mb-8">
+                                    <button className="border inline-flex gap-2 items-center border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
+                                        <IoIosTimer />{handleTimeRemaining(food.expired_datetime)} days left
                                     </button>
-                                    <button className="border border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
-                                        10 kg
-                                    </button>
-                                    <button className="border border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
-                                        25 kg
-                                    </button>
-                                    <button className="border border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
-                                        35 kg
+                                    <button className="col-span-2 inline-flex gap-2 items-center border border-gray-200 whitespace-nowrap  text-sm leading-6 py-2.5 rounded-xl px-5 text-center w-full font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
+                                        <MdShareLocation />{food.pickup_location}
                                     </button>
                                 </div>
                                 <div className="flex items-center flex-col min-[400px]:flex-row gap-3 mb-3 min-[400px]:mb-8">
                                     <div className=" flex items-center justify-center border border-gray-400 rounded-xl">
-                                        <button className="group py-[14px] px-3 w-full rounded-xl h-full flex items-center justify-center shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
+                                        <button onClick={handleChooseQtyMinus} className="group py-[14px] px-3 w-full rounded-xl h-full flex items-center justify-center shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
                                             <svg
                                                 className="stroke-black group-hover:stroke-black"
                                                 width={22}
@@ -172,9 +156,9 @@ const FoodDetails = () => {
                                         <input
                                             type="text"
                                             className="font-semibold  text-lg py-3 px-2 w-full min-[400px]:min-w-[75px] h-full bg-transparent placeholder: text-center hover:text-indigo-600 outline-0 hover:placeholder:text-indigo-600"
-                                            placeholder={1}
+                                            placeholder={qty}
                                         />
-                                        <button className="group py-[14px] px-3 w-full rounded-xl h-full flex items-center justify-center shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
+                                        <button onClick={handleChooseQtyPlus} className="group py-[14px] px-3 w-full rounded-xl h-full flex items-center justify-center shadow-sm shadow-transparent transition-all duration-300 hover:bg-gray-50 hover:shadow-gray-300">
                                             <svg
                                                 className="stroke-black group-hover:stroke-black"
                                                 width={22}
@@ -226,7 +210,7 @@ const FoodDetails = () => {
                                     </button>
                                 </div>
                                 <button className="text-center w-full px-5 py-4 rounded-xl bg-primary flex gap-2 items-center justify-center font-semibold text-lg text-white shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700 hover:shadow-indigo-300">
-                                    Buy Now <BiPurchaseTag />
+                                   Make a request <BiPurchaseTag />
                                 </button>
                             </div>
                         </div>
